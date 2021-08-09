4fe153b7e5639783df4a6b1d8a521cdf683c956f
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-09 16:34:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tarcio4lmeida/springbootmongodb/service/PostService.java
@@ -0,0 +1,23 @@
+package com.tarcio4lmeida.springbootmongodb.service;
+
+import java.util.Optional;
+
+import com.tarcio4lmeida.springbootmongodb.domain.Post;
+import com.tarcio4lmeida.springbootmongodb.repository.PostRepository;
+import com.tarcio4lmeida.springbootmongodb.service.exception.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+    public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
