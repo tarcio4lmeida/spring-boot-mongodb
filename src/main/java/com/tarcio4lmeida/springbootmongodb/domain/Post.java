7aa5dbb811d8dca49e6c78d1f1c16f5184ab3547
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-09 16:48:12 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/tarcio4lmeida/springbootmongodb/domain/Post.java
+++ src/main/java/com/tarcio4lmeida/springbootmongodb/domain/Post.java
@@ -2,9 +2,13 @@ package com.tarcio4lmeida.springbootmongodb.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.tarcio4lmeida.springbootmongodb.dto.AuthorDTO;
+import com.tarcio4lmeida.springbootmongodb.dto.CommentDTO;
+
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -19,6 +23,9 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO  author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
 	public Post() {
 	}
@@ -70,6 +77,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO  author) {
 		this.author = author;
+	}
+
+    public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
