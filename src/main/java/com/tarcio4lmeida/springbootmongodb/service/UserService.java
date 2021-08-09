a79ca4b3cdf8006d0f68d8837c8242c23a4c5ed2
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-09 15:04:45 -0300

    Inserção de usuário com POST

--- src/main/java/com/tarcio4lmeida/springbootmongodb/service/UserService.java
+++ src/main/java/com/tarcio4lmeida/springbootmongodb/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.tarcio4lmeida.springbootmongodb.domain.User;
+import com.tarcio4lmeida.springbootmongodb.dto.UserDTO;
 import com.tarcio4lmeida.springbootmongodb.repository.UserRepository;
 import com.tarcio4lmeida.springbootmongodb.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+    public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
